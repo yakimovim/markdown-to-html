8b303087e739ade4366e1b2a2716e400a4d24581
Ivan Yakimov <edlinsoft@gmail.com>
2022-11-22 09:21:37 +0300

    Command line support

---
+++ MarkdownToHtml.Tests/CodeProjectHtmlBuilderTests.cs
@@ -0,0 +1,144 @@
+namespace MarkdownToHtml.Tests
+{
+    public class CodeProjectHtmlBuilderTests
+    {
+        private readonly MarkdownConverter _converter;
+
+        public CodeProjectHtmlBuilderTests()
+        {
+            _converter = new MarkdownConverter(new CodeProjectHtmlBuilder());
+        }
+
+        [Fact]
+        public void Paragraph()
+        {
+            var html = _converter.BuildHtml("This is a paragraph text");
+
+            html.Should().Be($"<p>This is a paragraph text</p>{Environment.NewLine}");
+        }
+
+        [Theory]
+        [InlineData('*')]
+        [InlineData('_')]
+        public void Italic(char symbol)
+        {
+            var html = _converter.BuildHtml($"This is a {symbol}paragraph{symbol} text");
+
+            html.Should().Be($"<p>This is a <i>paragraph</i> text</p>{Environment.NewLine}");
+        }
+
+        [Theory]
+        [InlineData('*')]
+        [InlineData('_')]
+        public void Bold(char symbol)
+        {
+            var html = _converter.BuildHtml($"This is a {symbol}{symbol}paragraph{symbol}{symbol} text");
+
+            html.Should().Be($"<p>This is a <b>paragraph</b> text</p>{Environment.NewLine}");
+        }
+
+        
+        [Fact]
+        public void Strikethrough()
+        {
+            var html = _converter.BuildHtml($"This is a ~~paragraph~~ text");
+
+            html.Should().Be($"<p>This is a <s>paragraph</s> text</p>{Environment.NewLine}");
+        }
+
+        [Fact]
+        public void InlineCode()
+        {
+            var html = _converter.BuildHtml("This is a `paragraph` text");
+
+            html.Should().Be($"<p>This is a <code>paragraph</code> text</p>{Environment.NewLine}");
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void Headers(int level)
+        {
+            var html = _converter.BuildHtml(new string('#', level) + " This is a header");
+
+            html.Should().Be($"<h{level}>This is a header</h{level}>{Environment.NewLine}");
+        }
+
+        [Fact]
+        public void Link()
+        {
+            var html = _converter.BuildHtml("This is a [link](https://www.example.com) text");
+
+            html.Should().Be($"<p>This is a <a href='https://www.example.com' rel='nofollow' target='_blank'>link</a> text</p>{Environment.NewLine}");
+        }
+
+        [Fact]
+        public void UnorderedList()
+        {
+            var html = _converter.BuildHtml(@"
+* Item 1
+* Item 2
+* Item 3");
+
+            html.Should().Be($@"<ul>
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+</ul>{Environment.NewLine}");
+        }
+
+        [Fact]
+        public void OrderedList()
+        {
+            var html = _converter.BuildHtml(@"
+1. Item 1
+1. Item 2
+1. Item 3");
+
+            html.Should().Be($@"<ol>
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+</ol>{Environment.NewLine}");
+        }
+
+        [Fact]
+        public void HorizontalLine()
+        {
+            var html = _converter.BuildHtml("---");
+
+            html.Should().Be($"<hr/>{Environment.NewLine}");
+        }
+
+        [Fact]
+        public void CodeBlockCs()
+        {
+            var html = _converter.BuildHtml(@"```cs
+public void AddList(List<string> list)
+{
+    Console.WriteLine(""List is added"")
+}
+```");
+
+            html.Should().Be($@"<pre lang='cs'>
+public void AddList(List&lt;string&gt; list)
+{{
+    Console.WriteLine(&quot;List is added&quot;)
+}}
+</pre>{Environment.NewLine}");
+        }
+
+        [Fact]
+        public void CodeBlockText()
+        {
+            var html = _converter.BuildHtml(@"```
+This is an ""error"" message
+```");
+
+            html.Should().Be($@"<pre lang='text'>
+This is an &quot;error&quot; message
+</pre>{Environment.NewLine}");
+        }
+    }
+}
---
+++ MarkdownToHtml/ConverterCommand.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel.DataAnnotations;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace MarkdownToHtml
+{
+    internal enum TargetTypes
+    {
+        Google,
+        CodeProject
+    }
+
+    internal class ConverterCommand
+    {
+        private readonly IConsole _console;
+
+        [Argument(0)]
+        [Required]
+        public string InputMarkdownFile { get; }
+
+        [Option("-o|--output")]
+        [Required]
+        public string OutputHtmlFile { get; set; }
+
+        [Option]
+        [Required]
+        public TargetTypes Target { get; set; }
+
+        public ConverterCommand(IConsole console)
+        {
+            _console = console ?? throw new ArgumentNullException(nameof(console));
+        }
+
+        private void OnExecute()
+        {
+            if(!File.Exists(InputMarkdownFile))
+            {
+                _console.WriteLine($"Input file '{InputMarkdownFile}' does not exist.");
+                return;
+            }
+
+            if(File.Exists(OutputHtmlFile))
+            {
+                if(!Prompt.GetYesNo($"Output file '{OutputHtmlFile}' already exists. Do you want to override it?", false))
+                {
+                    return;
+                }
+            }
+
+            HtmlBuilder? htmlBuilder = Target switch
+            {
+                TargetTypes.Google => new GoogleBloggerHtmlBuilder(),
+                TargetTypes.CodeProject => new CodeProjectHtmlBuilder(),
+                _ => null
+            };
+
+            if(htmlBuilder == null)
+            {
+                _console.WriteLine($"Unknown target type '{Target}'.");
+                return;
+            }
+
+            var converter = new MarkdownConverter(htmlBuilder);
+
+            var markdown = File.ReadAllText(InputMarkdownFile);
+
+            var html = converter.BuildHtml(markdown);
+
+            File.WriteAllText(OutputHtmlFile, html);
+        }
+    }
+}
